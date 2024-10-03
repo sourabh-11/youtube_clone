220d6f179da5f43b78dc13eb48263a6dab6c14f9
sourabh <sourabhjangid2002@gmail.com>
2024-10-03 08:12:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Componenets/Navbar/Navbar'
+import {Route,Routes}   from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+       <Routes>
+        <Route path = '/' element ={<Home/>}  />
+        <Route path = '/video/:categoryId/:videoId' element ={<Video/>} />
+
+       </Routes>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Video/Video.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Video.css'
+
+const Video = () => {
+  return (
+    <div>
+        <h1>heloo</h1>
+    </div>
+  )
+}
+
+export default Video
